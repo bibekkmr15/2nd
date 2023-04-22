0f3671d523b55af59f7da5a0f1dfb1f2e3808feb
Bibek Kumar Buranwal <bibekkmr15@gmail.com>
2023-04-22 19:18:06 +0530

    connect to mongoose database for test

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 const express = require("express");
-const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
+const Campground = require("./models/campground");
+
+// connect to mongoose
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+  console.log("Database connected");
+}
+
+const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-// app.get("/", (req, res) => {
-//   res.send("it's working");
-// });
-
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/makecampground", async (req, res) => {
+  const camp = new Campground({
+    title: "my Backyard",
+    description: "cheap camping",
+  });
+  await camp.save();
+  res.send(camp);
 });
 
 app.listen(3000, () => {
