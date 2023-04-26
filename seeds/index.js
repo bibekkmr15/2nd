4f6556f29abe72e3e0799c022a0d0e58f958a6a9
Bibek Kumar Buranwal <bibekkmr15@gmail.com>
2023-04-26 18:44:37 +0530

    add some style (partials, navbar) and images using bootstrap

--- seeds/index.js
+++ seeds/index.js
@@ -31,9 +31,15 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 1000) + 500;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${randArrayElement(descriptors)} ${randArrayElement(places)}`,
+      // image: "https://source.unsplash.com/collection/483251",
+      image: "https://source.unsplash.com/collection/483251/900x450",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium cupiditate distinctio hic, at temporibus ea dicta repellendus est veritatis nisi nemo nihil dolorum doloremque harum accusamus reprehenderit, voluptatum eum cum.",
+      price,
     });
     await camp.save();
   }
