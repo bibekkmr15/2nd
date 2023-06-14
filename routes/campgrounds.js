6a6110e7abe3f245ccb83dfccc1121f9bf0f40e9
Bibek Kumar Buranwal <bibekkmr15@gmail.com>
2023-06-14 20:19:46 +0530

    Image Upload - Storing upload images link in Mongo and Display the images

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -10,15 +10,21 @@ const upload = multer({ storage });
 router
   .route("/")
   .get(catchAsync(campgrounds.index))
-  // .post(
-  //   isLoggedIn,
-  //   validateCampground,
-  //   catchAsync(campgrounds.createCampground)
-  // );
-  .post(upload.array("image"), (req, res) => {
-    console.log(req.body, req.files);
-    res.send("it worked");
-  });
+  // // Now, our validation got screwed up. The main issue here is actually that Validate Campground is running before
+  // // we run Multer Middleware. And Multer is responsible for adding the data onto request.body. This (validateCampground)
+  // // depends on request.body.
+  // .post(isLoggedIn, validateCampground, upload.array("image"), catchAsync(campgrounds.createCampground));
+
+  // In a production environment, like in the real world, we definitely don't want to just upload our images before
+  // we validate the other data. But the way that Multer works is that it's going to first upload everything. And while
+  // it's parsing, then it sends us the past body and the files and everything that we have access to and request our
+  // bodies. So we'll have to figure out some other option eventually.
+  .post(
+    isLoggedIn,
+    upload.array("image"),
+    validateCampground,
+    catchAsync(campgrounds.createCampground)
+  );
 
 // "/campgrounds/new" route before "/campgrounds/:id"
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
