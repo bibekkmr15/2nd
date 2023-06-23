a07c444ec18867b17a2e83251e10405a65c081fd
Bibek Kumar Buranwal <bibekkmr15@gmail.com>
2023-06-23 19:23:16 +0530

    Adding Map - Display a map in show campground page

---
+++ public/javascripts/showPageMap.js
@@ -0,0 +1,8 @@
+//    mapbox map integration
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+  container: "map", // container ID
+  style: "mapbox://styles/mapbox/streets-v12", // style URL
+  center: [-74.5, 40], // starting position [lng, lat]
+  zoom: 9, // starting zoom
+});
